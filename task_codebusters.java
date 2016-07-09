45ee451f5277f01e10336c3ed435ded557243751
javadev <javadev75@gmail.com>
2016-07-09 21:33:44 +0300

    Add codebusters Bronze solution.

--- task_codebusters.java
+++ task_codebusters.java
@@ -12,12 +12,14 @@ class Player {
     public static int MAX_BUST_RANGE = 1760;
     public static int MIN_BUST_RANGE = 900;
     public static int MAX_RELEASE_RANGE = 1600;
-
+    public static int VIEW_RANGE = 2200;
+    
     private static Entity base;
+    private static int bustersPerPlayer;
     
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
-        int bustersPerPlayer = in.nextInt(); // the amount of busters you control
+        bustersPerPlayer = in.nextInt(); // the amount of busters you control
         int ghostCount = in.nextInt(); // the amount of ghosts on the map
         int myTeamId = in.nextInt(); // if this is 0, your base is on the top left of the map, if it is one, on the bottom right
         
@@ -31,8 +33,8 @@ class Player {
         while (true) {
             int entities = in.nextInt(); // the number of busters and ghosts visible to you
             System.err.println("I can see " + entities + " entities");
-            List<Entity> myBuster = new ArrayList<>();
-            List<Entity> enBuster = new ArrayList<>();
+            List<Entity> myBusters = new ArrayList<>();
+            List<Entity> ennBusters = new ArrayList<>();
             List<Entity> ghosts = new ArrayList<>();
             for (int i = 0; i < entities; i++) {
                 int entityId = in.nextInt(); // buster id or ghost id
@@ -43,39 +45,28 @@ class Player {
                 int value = in.nextInt(); // For busters: Ghost id being carried. For ghosts: number of busters attempting to trap this ghost.
                 Entity e = new Entity(entityId, x, y, entityType, state, value);
                 if (entityType != -1) {
-                    if (entityType == myTeamId) myBuster.add(e);
-                    else enBuster.add(e);                   
+                    if (entityType == myTeamId) {
+                        //Modify the id to be able to have number from 0 to bustersPerPlayer for my players
+                        e.id = entityType == 0 ? entityId : entityId - bustersPerPlayer;
+                        myBusters.add(e);
+                    } else ennBusters.add(e);                   
                 } else ghosts.add(e);
             }
-            System.err.println("myBuster size : " + myBuster.size());
-            System.err.println("ennBuster size : " + enBuster.size());
+            System.err.println("myBuster size : " + myBusters.size());
+            System.err.println("ennBuster size : " + ennBusters.size());
             System.err.println("ghosts size : " + ghosts.size());
             for (int i = 0; i < bustersPerPlayer; i++) {
-                Entity currentBuster = myBuster.get(i);
-                Action action = getAction(currentBuster, ghosts);
+                Entity currentBuster = myBusters.get(i);
+                Action action = getAction(currentBuster, ghosts, ennBusters);
                 action.print();
             }
         }
     }
 
-    private static Action getAction(Entity currentBuster, List<Entity> ghosts) {
-        if (currentBuster.state == 0) {
-            if (!ghosts.isEmpty()) {
-                Entity closestGhost = getClosestGhost(currentBuster, ghosts);
-                System.err.println("Closest ghost for buster : "
-                        + currentBuster.id + " is ghost : " + closestGhost.id);
-                Boolean isBustingPossible = isBustingPossible(currentBuster,
-                        closestGhost);
-                System.err.println("isBustPossible : " + isBustingPossible);
-                if (isBustingPossible) {
-                    return new Action("BUST", closestGhost.id);
-                } else {
-                    return new Action("MOVE", closestGhost.x, closestGhost.y);
-                }
-            } else {
-                return discoverBase();
-            }
-        } else {
+    private static Action getAction(Entity currentBuster, List<Entity> ghosts,
+            List<Entity> ennBusters) {
+        Action a = null;
+        if (currentBuster.state == 1) {
             // Buster with ghost need to return to base
             Boolean isReleasePossible = isRealeasePossible(currentBuster, base);
             if (isReleasePossible) {
@@ -84,30 +75,77 @@ class Player {
                 return new Action("MOVE", base.x, base.y);
             }
         }
+        
+        //Get action to stun an ennemy with a ghost
+        a = getEnnemy(currentBuster, ennBusters);
+        if (a == null) {
+            //Get action to bust or move to catch a ghost
+            a = getGhost(currentBuster, ghosts);
+            if (a == null) {
+                //if no action possible go discover base;
+                return discoverBase(currentBuster);             
+            }
+        }
+        
+        return a;
     }
 
-    private static Action discoverBase() {
-        //Random direction for testing
-        Random r = new Random();
-        int x = r.nextInt(MAP_WIDTH - 0 + 1) + 0;
-        int y = r.nextInt(MAP_HEIGHT - 0 + 1) + 0;
-    
-        return new Action("MOVE", x, y);
+    private static Action getEnnemy(Entity buster, List<Entity> ennemies) {
+        if (!ennemies.isEmpty()) {
+            Entity closestEnnemy = getClosestEntity(buster, ennemies);
+            if (closestEnnemy!= null && closestEnnemy.state == 1) {
+                return new Action("STUN", closestEnnemy.id);
+            }
+        }
+        return null;
     }
 
-    private static Entity getClosestGhost(Entity currentBuster, List<Entity> ghosts) {
+    private static Action getGhost(Entity currentBuster, List<Entity> ghosts) {
+        if (!ghosts.isEmpty()) {
+            Entity closestGhost = getClosestEntity(currentBuster, ghosts);
+            if (closestGhost == null) {
+                System.err.println("No closest ghost for buster : " + currentBuster.id);
+                return null;
+            }
+            System.err.println("Closest ghost for buster : " + currentBuster.id + " is ghost : " + closestGhost.id);
+            Boolean isBustingPossible = isBustingPossible(currentBuster, closestGhost);
+            System.err.println("isBustPossible : " + isBustingPossible);
+            if (isBustingPossible) {
+                return new Action("BUST", closestGhost.id);
+            } else {
+                return new Action("MOVE", closestGhost.x, closestGhost.y);
+            }
+        }
+        
+        return null;
+    }
+
+    private static Action discoverBase(Entity buster) {
+        double discoverZoneSize = MAP_HEIGHT / bustersPerPlayer;
+        int posYInZone = (int) ((discoverZoneSize * (buster.id + 1)) - (discoverZoneSize / 2));
+        System.err.println("For buster : " + buster.id + " posInZone is : " + posYInZone);
+        if (buster.y != posYInZone) {
+            return new Action("MOVE", buster.x, posYInZone);
+        } 
+        
+        int posXInZone = base.x == 0 ? MAP_WIDTH : 0;
+        return new Action ("MOVE", posXInZone, posYInZone);
+    }
+
+    private static Entity getClosestEntity(Entity buster, List<Entity> entities) {
         double minDist = Double.MAX_VALUE;
-        Entity closestGhost = null; 
-        for (Entity ghost : ghosts) {
-            double distance = getDistance(currentBuster, ghost);
-            System.err.println("Distance for buster : " + currentBuster.id + " and ghost : " + ghost.id);
-            System.err.println("Distance ====> " + distance);
+        Entity closestEntity = null; 
+        for (Entity entity : entities) {
+            double distance = getDistance(buster, entity);
+            //if distance is greater than view_range that's mean that our buster is too far away
+            //if the entity is a ghost and the distance is < than MIN_BUST_RANGE then we can't catch it
+            if (distance > VIEW_RANGE || (entity.type == -1 && distance < MIN_BUST_RANGE)) continue;
             if (distance < minDist) {
-                closestGhost = ghost;
+                closestEntity = entity;
                 minDist = distance;
             }
         }
-        return closestGhost;
+        return closestEntity;
     }
 
     private static double getDistance(Entity a, Entity b) {
